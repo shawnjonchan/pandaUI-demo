f1dbd27a7b3dcaa37afada73e3341efaeb2a6f07
shawnjonchan <xiaojiechen86@gmail.com>
2020-12-08 19:17:22 +0800

    添加 input 的测试用例

--- src/app.js
+++ src/app.js
@@ -15,5 +15,17 @@ new Vue({
         loading1:false,
         loading2:true,
         loading3:false,
+    },
+    created(){
+        setTimeout(()=>{
+            let event = new Event('change')
+            let inputElement = this.$el.querySelector('input')
+            inputElement.dispatchEvent(event)
+        },3000)
+    },
+    methods:{
+        inputChange(e){
+           console.log(e)
+        }
     }
 })
