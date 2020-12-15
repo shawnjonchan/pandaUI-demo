f31168e9aa8920830b1e1a89437b9e3c25a6e0a3
shawnjonchan <xiaojiechen86@gmail.com>
2020-12-15 16:55:05 +0800

    基本功能完成

---
+++ src/sider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sider">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'PandaSider'
+}
+</script>
+
+<style lang="scss" scoped>
+   .sider {
+
+   }
+
+</style>
