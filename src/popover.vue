97aac831da5d12ef29cc2c23e71b5c8456af32a3
shawnjonchan <xiaojiechen86@gmail.com>
2020-12-23 16:38:31 +0800

    解决抖动 bug

--- src/popover.vue
+++ src/popover.vue
@@ -145,6 +145,7 @@
     margin-top: -10px;
     &::before, &::after{
       left: 10px;
+      border-bottom: none;
     }
     &::before {
       border-top-color: black;
@@ -159,6 +160,7 @@
     margin-top: 10px;
     &::before, &::after {
       left: 10px;
+      border-top: none;
     }
     &::before {
       border-bottom-color: black;
@@ -175,6 +177,7 @@
     &::before, &::after{
       transform: translateY(-50%);
       top: 50%;
+      border-right: none;
     }
     &::before{
       border-left-color: black;
@@ -190,6 +193,7 @@
     &::before, &::after{
       transform: translateY(-50%);
       top: 50%;
+      border-left: none;
     }
     &::before {
       border-right-color: black;
