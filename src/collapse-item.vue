ae2698d4dfef88e9e6a476abeb58c0a8119af2a8
shawnjonchan <xiaojiechen86@gmail.com>
2020-12-23 21:01:24 +0800

    添加 single 选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open=!open">
+    <div class="title" @click="toggle">
         {{title}}
     </div>
     <div class="content" v-if="open">
@@ -22,6 +22,27 @@ export default {
        return {
            open: false
        }
+   },
+   inject:['eventBus'],
+   mounted (){
+       this.eventBus && this.eventBus.$on('update:selected',(vm) =>{
+           if(vm !== this){
+               this.close()
+           }
+       })
+   },
+   methods:{
+       toggle(){
+         if(this.open){
+            this.open = false
+         } else {
+             this.open = true
+             this.eventBus && this.eventBus.$emit('update:selected',this)
+         }
+       },
+       close(){
+         this.open = false
+       }
    }
 }
 </script>
@@ -31,30 +52,15 @@ export default {
   $border-radius: 4px;
   .collapseItem {
     > .title{
-        border: 1px solid $grey;
-        margin-top: -1px;
-        margin-left: -1px;
-        margin-right: -1px;
-        min-height: 32px;
-        display: flex;
-        align-items: center;
-        padding: 0 8px;
+        border: 1px solid $grey; margin-top: -1px; margin-left: -1px; margin-right: -1px; min-height: 32px; display: flex; align-items: center; padding: 0 8px;
     }
     &:first-child {
-      > .title {
-        border-top-left-radius: $border-radius;
-        border-top-right-radius: $border-radius;
-      }
+      > .title { border-top-left-radius: $border-radius; border-top-right-radius: $border-radius; }
     }
     &:last-child {
-      > .title:last-child {
-        border-bottom-left-radius: $border-radius;
-        border-bottom-right-radius: $border-radius;
-      }
+      > .title:last-child { border-bottom-left-radius: $border-radius; border-bottom-right-radius: $border-radius; }
     }
-    > .content {
-        padding: 8px;
-    }
+    > .content { padding: 8px; }
   }
 
 </style>
